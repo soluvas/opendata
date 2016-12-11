8866cdfaa12b1b12a1995ece80f0a511df5cfe2c
Hendy Irawan <ceefour666@gmail.com>
2016-12-11 22:43:14 +0700

    Create and Import SQL

---
+++ wikidata/locality.id.create.sql
@@ -0,0 +1,19 @@
+CREATE TABLE`locality` (
+  `id` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `geonamesId` VARCHAR(255) NULL,
+  `place` VARCHAR(255) NOT NULL,
+  `stateId` VARCHAR(255) NOT NULL,
+  `stateIso` VARCHAR(255) NOT NULL,
+  `countryId` VARCHAR(255) NOT NULL,
+  `countryIso` VARCHAR(2) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `name` (`name` ASC),
+  INDEX `geonamesId` (`geonamesId` ASC),
+  INDEX `place` (`place` ASC),
+  INDEX `stateId` (`stateId` ASC),
+  INDEX `stateIso` (`stateIso` ASC),
+  INDEX `countryId` (`countryId` ASC),
+  INDEX `countryIso` (`countryIso` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
